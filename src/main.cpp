ba0248f3ba36f76c4e34be2cf7f798eee36d1665
Sandro Pischinger <mail@sandropischinger.de>
2023-11-27 22:25:58 +0100

    use c++17 standard

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,30 @@
 #include <Arduino.h>
 #include <TMCStepper.h>
 
-// source: https://github.com/teemuatlut/TMCStepper/issues/178
+#include "door_actuator.h"
 
-#define EN_PIN 19 // Enable
-// #define DIR_PIN 5 // Direction
-#define STEP_PIN 18         // Step
-#define STALL_PIN_X 5      // Teensy pin that diag pin is attached to
-#define SERIAL_PORT Serial2 // HardwareSerial port for Teensy 4.0
-#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
-#define R_SENSE 0.11f       // Match to your driver
+// source: https://github.com/teemuatlut/TMCStepper/issues/178
 
 // higher value of STALL_VALUE increases stall sensitivity
 // diag pin pulsed HIGH when SG_RESULT falls below 2*STALL_VALUE
 // must be in StealthChop Mode for stallguard to work
 // Value of TCOOLTHRS must be greater than TSTEP & TPWMTHRS
-#define STALL_VALUE 12 // [0..255]
+constexpr auto EN_PIN{19};
+constexpr auto STEP_PIN{18};
+constexpr auto STALL_PIN{5};
+constexpr auto DRIVER_ADDRESS{0b00};
+constexpr auto R_SENSE{0.11f};
+constexpr auto STALL_VALUE{12}; // [0..255], higher -> increases stall sensitivity
+
+auto SERIAL_PORT{Serial2};
 int stepTime = 160;
 
 TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
 bool shaftVal = false;
-bool stalled_X = false;
+bool stalled = false;
 
 void stallInterruptX() { // flag set when motor stalls
-  stalled_X = true;
+  stalled = true;
 }
 
 void run_until_stalled(bool dir);
@@ -32,7 +33,7 @@ void setup() {
   pinMode(EN_PIN, OUTPUT);
   pinMode(STEP_PIN, OUTPUT);
   // shaft direction controlled through uart: driver.shaft(true or false)
-  pinMode(STALL_PIN_X, INPUT);
+  pinMode(STALL_PIN, INPUT);
 
   Serial.begin(115200);
   SERIAL_PORT.begin(115200); // HW UART drivers
@@ -53,7 +54,7 @@ void setup() {
   // TCOOLTHRS needs to be set for stallgaurd to work //
   driver.TCOOLTHRS(0xFFFFF); // 20bit max
   driver.SGTHRS(STALL_VALUE);
-  attachInterrupt(digitalPinToInterrupt(STALL_PIN_X), stallInterruptX, RISING);
+  attachInterrupt(digitalPinToInterrupt(STALL_PIN), stallInterruptX, RISING);
   digitalWrite(EN_PIN, LOW); // Enable driver in hardware
 }
 
@@ -71,13 +72,13 @@ void run_until_stalled(bool dir) {
   Serial.println("running until stalled");
   digitalWrite(EN_PIN, LOW);
   driver.shaft(dir);
-  while (!stalled_X) {
+  while (!stalled) {
     digitalWrite(STEP_PIN, HIGH);
     delayMicroseconds(delay);
     digitalWrite(STEP_PIN, LOW);
     delayMicroseconds(delay);
   }
-  stalled_X = false;
+  stalled = false;
   digitalWrite(EN_PIN, HIGH);
   Serial.println("stalled");
 }
