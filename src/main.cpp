15f18d23c5715fd51c09846b99700224d4d17fab
Sandro Pischinger <mail@sandropischinger.de>
2023-11-27 22:51:41 +0100

    tune parameters

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,9 @@ constexpr auto STEP_PIN{18};
 constexpr auto STALL_PIN{5};
 constexpr auto DRIVER_ADDRESS{0b00};
 constexpr auto R_SENSE{0.11f};
-constexpr auto STALL_VALUE{12}; // [0..255], higher -> increases stall sensitivity
+constexpr auto STALL_VALUE{6}; // [0..255], higher -> increases stall sensitivity
 
 auto SERIAL_PORT{Serial2};
-int stepTime = 160;
 
 TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
 bool shaftVal = false;
@@ -38,21 +37,25 @@ void setup() {
   Serial.begin(115200);
   SERIAL_PORT.begin(115200); // HW UART drivers
 
-  driver.begin(); // SPI: Init CS pins and possible SW SPI pins
-  driver.toff(4); // Enables driver in software, changed from 5
+  driver.begin();
+  driver.toff(4);
   driver.blank_time(24);
-  driver.rms_current(600); // Set motor RMS current
-  driver.microsteps(16);   // Set microsteps to 1/16th
+  driver.I_scale_analog(false);
+  driver.internal_Rsense(false); 
+  driver.mstep_reg_select(true);
+  driver.rms_current(400);
+  driver.microsteps(64);
 
   // driver.en_pwm_mode(true); // Toggle stealthChop on TMC2130/2160/5130/5160
-  // driver.en_spreadCycle(false); // Toggle spreadCycle on TMC2208/2209/2224
-  driver.pwm_autoscale(true); // Needed for stealthChop
-  driver.semin(5);
+  driver.en_spreadCycle(false);
+  driver.pwm_autoscale(true);
+  driver.pdn_disable(false);
+  driver.semin(0);
   driver.semax(2);
   driver.sedn(0b01);
   driver.shaft(shaftVal);
-  // TCOOLTHRS needs to be set for stallgaurd to work //
-  driver.TCOOLTHRS(0xFFFFF); // 20bit max
+  driver.TCOOLTHRS(0xFFFFF);
+  driver.TPWMTHRS(0);
   driver.SGTHRS(STALL_VALUE);
   attachInterrupt(digitalPinToInterrupt(STALL_PIN), stallInterruptX, RISING);
   digitalWrite(EN_PIN, LOW); // Enable driver in hardware
@@ -68,7 +71,7 @@ void loop() {
 }
 
 void run_until_stalled(bool dir) {
-  int delay = 500;
+  int delay = 50;
   Serial.println("running until stalled");
   digitalWrite(EN_PIN, LOW);
   driver.shaft(dir);
